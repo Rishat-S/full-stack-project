3b0a4df588e924eb516bf1371daa418288a0a8aa
Rishat Suleymanov <spk_irbis@mail.ru>
2021-09-30 20:51:10 +0300

    Entity files edited

--- src/main/java/com/example/demogol/entity/File.java
+++ src/main/java/com/example/demogol/entity/File.java
@@ -1,10 +1,33 @@
 package com.example.demogol.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
 public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
     private byte[] fileBytes;
     private Long size;
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createDate = LocalDateTime.now();
+    }
 
 }
--- src/main/java/com/example/demogol/entity/User.java
+++ src/main/java/com/example/demogol/entity/User.java
@@ -1,26 +1,48 @@
 package com.example.demogol.entity;
 
 import com.example.demogol.entity.enums.Role;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(unique = true, updatable = false)
     private String username;
     private String lastname;
+    @Column(unique = true)
     private String email;
-    private String bio;
+    @Column(length = 3000)
     private String password;
 
-    private Set<Role> role = new HashSet<>();
+    @ElementCollection(targetClass = Role.class)
+    @CollectionTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user", orphanRemoval = true)
     private List<File> files = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createDate;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User() {
+
+    }
 
     @PrePersist
     protected void onCreate() {
