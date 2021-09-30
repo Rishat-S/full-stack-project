b67852195ddaae251e78c0469d25fbd78c22f2dd
Rishat Suleymanov <spk_irbis@mail.ru>
2021-09-30 19:58:18 +0300

    File.java edited

--- src/main/java/com/example/demogol/entity/File.java
+++ src/main/java/com/example/demogol/entity/File.java
@@ -1,19 +1,10 @@
 package com.example.demogol.entity;
 
-import javax.persistence.PrePersist;
-import java.time.LocalDateTime;
-
 public class File {
     private Long id;
     private String name;
-    private String size;
-
-    private User user;
-    private LocalDateTime createDate;
-
-    @PrePersist
-    protected void onCreate() {
-        this.createDate = LocalDateTime.now();
-    }
+    private byte[] fileBytes;
+    private Long size;
+    private Long userId;
 
 }
