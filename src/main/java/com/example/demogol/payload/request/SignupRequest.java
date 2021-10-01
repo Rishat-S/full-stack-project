a6f167339c80b60d98f68fdb2291c88a1c30e3fd
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-01 22:50:51 +0300

    Added annotations and validators

---
+++ src/main/java/com/example/demogol/payload/request/SignupRequest.java
@@ -0,0 +1,30 @@
+package com.example.demogol.payload.request;
+
+import com.example.demogol.annotation.PasswordMatches;
+import com.example.demogol.annotation.ValidEmail;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+@PasswordMatches
+public class SignupRequest {
+
+    @Email(message = "It should have email format")
+    @NotBlank(message = "User email is required")
+    @ValidEmail
+    private String email;
+    @NotEmpty(message = "Please enter your name")
+    private String name;
+    @NotEmpty(message = "Please enter your lastname")
+    private String lastname;
+    @NotEmpty(message = "Please enter your username")
+    private String username;
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6)
+    private String password;
+    private String confirmPassword;
+}
---
+++ src/main/java/com/example/demogol/validations/PasswordMatchesValidator.java
@@ -0,0 +1,21 @@
+package com.example.demogol.validations;
+
+import com.example.demogol.annotation.PasswordMatches;
+import com.example.demogol.payload.request.SignupRequest;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext context) {
+        SignupRequest userSignupRequest = (SignupRequest) obj;
+        return userSignupRequest.getPassword().equals(userSignupRequest.getConfirmPassword());
+    }
+}
