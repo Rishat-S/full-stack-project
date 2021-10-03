d4c3ac2793b562742903866c40927b21c49c8e05
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-03 20:33:06 +0300

    VessageResponse and AuthController added

--- src/main/java/com/example/demogol/web/AuthController.java
+++ src/main/java/com/example/demogol/web/AuthController.java
@@ -1,14 +1,46 @@
 package com.example.demogol.web;
 
+import com.example.demogol.payload.request.SignupRequest;
+import com.example.demogol.payload.response.MessageResponse;
+import com.example.demogol.security.JWTTokenProvider;
+import com.example.demogol.service.UserService;
+import com.example.demogol.validations.ResponseErrorValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/auth")
 @PreAuthorize("permitAll()")
 public class AuthController {
+    @Autowired
+    private JWTTokenProvider jwtTokenProvider;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private ResponseErrorValidation responseErrorValidation;
+    @Autowired
+    private UserService userService;
+
+    public ResponseEntity<Object> registerUser(@Valid @RequestBody SignupRequest signupRequest, BindingResult bindingResult) {
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (ObjectUtils.isEmpty(errors)) {
+            return errors;
+        }
+
+        userService.createUser(signupRequest);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully"));
+    }
 
 }
