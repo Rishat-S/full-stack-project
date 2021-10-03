1ae7145840482600033834aae86d1ac5e662c5eb
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-03 20:07:43 +0300

    UserExistException added

--- src/main/java/com/example/demogol/service/UserService.java
+++ src/main/java/com/example/demogol/service/UserService.java
@@ -1,12 +1,45 @@
 package com.example.demogol.service;
 
+import com.example.demogol.entity.User;
+import com.example.demogol.entity.enums.Role;
+import com.example.demogol.exceptions.UserExistException;
+import com.example.demogol.payload.request.SignupRequest;
+import com.example.demogol.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     public static final Logger LOG = LoggerFactory.getLogger(UserService.class);
 
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(SignupRequest userIn) throws UserExistException {
+        User user = new User();
+        user.setEmail(userIn.getEmail());
+        user.setName(userIn.getName());
+        user.setLastname(userIn.getLastname());
+        user.setUsername(userIn.getUsername());
+        user.setPassword(passwordEncoder.encode(userIn.getPassword()));
+        user.getRoles().add(Role.ROLE_USER);
+
+        try {
+            LOG.info("Saving user {}", userIn.getEmail());
+            return userRepository.save(user);
+        } catch (Exception e) {
+            LOG.error("Error during registration. {}", e.getMessage());
+            throw new UserExistException("The user " + user.getUsername() + " already exist. Please check credentials");
+        }
+    }
 
 }
