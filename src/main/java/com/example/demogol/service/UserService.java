833d0c4fdd4f98e61d984fd9e593c245cff36f4b
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-03 19:05:50 +0300

    Added UserService

---
+++ src/main/java/com/example/demogol/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.demogol.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+
+}
