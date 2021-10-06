00e49e1e94ae07300239189cc53b6c3745180e44
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-06 23:01:30 +0300

    JWTTokenSuccessResponse added

--- src/main/java/com/example/demogol/web/AuthController.java
+++ src/main/java/com/example/demogol/web/AuthController.java
@@ -1,14 +1,20 @@
 package com.example.demogol.web;
 
+import com.example.demogol.payload.request.LoginRequest;
 import com.example.demogol.payload.request.SignupRequest;
+import com.example.demogol.payload.response.JWTTokenSuccessResponse;
 import com.example.demogol.payload.response.MessageResponse;
 import com.example.demogol.security.JWTTokenProvider;
+import com.example.demogol.security.SecurityConstants;
 import com.example.demogol.service.UserService;
 import com.example.demogol.validations.ResponseErrorValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.ObjectUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -28,12 +34,30 @@ public class AuthController {
     private ResponseErrorValidation responseErrorValidation;
     @Autowired
     private UserService userService;
+
+    public ResponseEntity<Object> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult bindingResult) {
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (!ObjectUtils.isEmpty(errors)) {
+            return errors;
+        }
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(),
+                loginRequest.getPassword()
+        ));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = SecurityConstants.TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTTokenSuccessResponse(true, jwt));
+
+    }
     
 
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(@Valid @RequestBody SignupRequest signupRequest, BindingResult bindingResult) {
         ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
-        if (ObjectUtils.isEmpty(errors)) {
+        if (!ObjectUtils.isEmpty(errors)) {
             return errors;
         }
 
