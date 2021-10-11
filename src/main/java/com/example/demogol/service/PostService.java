b7ed4346c433ae17b7eb6296fff72321271c524f
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-11 11:44:17 +0300

    PostService and PostNotFoundException added

---
+++ src/main/java/com/example/demogol/service/PostService.java
@@ -0,0 +1,64 @@
+package com.example.demogol.service;
+
+import com.example.demogol.dto.PostDTO;
+import com.example.demogol.entity.Post;
+import com.example.demogol.entity.User;
+import com.example.demogol.exceptions.PostNotFoundException;
+import com.example.demogol.repository.ImageRepository;
+import com.example.demogol.repository.PostRepository;
+import com.example.demogol.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.List;
+
+@Service
+public class PostService {
+    public static final Logger LOG = LoggerFactory.getLogger(PostService.class);
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final ImageRepository imageRepository;
+
+    public PostService(PostRepository postRepository, UserRepository userRepository, ImageRepository imageRepository) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+        this.imageRepository = imageRepository;
+    }
+
+    public Post createPost(PostDTO postDTO, Principal principal) {
+        User user = getUserByPrincipal(principal);
+        Post post = new Post();
+        post.setUser(user);
+        post.setCaption(postDTO.getCaption());
+        post.setLocation(postDTO.getLocation());
+        post.setTitle(postDTO.getTitle());
+        post.setLikes(0);
+
+        LOG.info("Saving Post for User: {}", user.getEmail());
+        return postRepository.save(post);
+
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAllByOrderByCreatedDateDesc();
+    }
+
+    public Post getPostById(Long postId, Principal principal) {
+        User user = getUserByPrincipal(principal);
+
+        return postRepository.findPostByIdAndUser(postId, user)
+                .orElseThrow(() -> new PostNotFoundException("Post cannot be found for username; " + user.getEmail()))
+                ;
+    }
+
+    private User getUserByPrincipal(Principal principal) {
+        String username = principal.getName();
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found with username " + username));
+    }
+
+}
