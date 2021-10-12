32b430f267a17b9c3ed65c60fd1b2844a1ed84eb
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-12 19:25:51 +0300

    commentFacade added

---
+++ src/main/java/com/example/demogol/facade/CommentFacade.java
@@ -0,0 +1,18 @@
+package com.example.demogol.facade;
+
+import com.example.demogol.dto.CommentDTO;
+import com.example.demogol.entity.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentFacade {
+
+    public CommentDTO commentToCommentDTO(Comment comment) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setUsername(comment.getUsername());
+        commentDTO.setMessage(comment.getMessage());
+
+        return commentDTO;
+    }
+}
